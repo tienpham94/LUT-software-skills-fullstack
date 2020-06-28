5a44c1d737d7a8ba3fed72e2edf91dba17eb646e
Tien Pham <tiendev94@gmail.com>
2020-06-28 20:19:01 +0300

    Update app

--- exercises-coursework/express-customerApp/app.js
+++ exercises-coursework/express-customerApp/app.js
@@ -4,6 +4,29 @@ var path = require("path");
 
 var app = express();
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+// var logger = function(req, res, next) {
+//   console.log("logging...");
+//   next();
+// };
+
+// app.use(logger);
+
+// Body Parser Middleware
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+// Set Static Path
+app.use(express.static(path.join(__dirname, 'public')))
+
+var person = {
+  name: "Jeff",
+  age: 30
+}
+
+app.get("/", function(req, res) {
+  res.json(person);
+});
+
+app.listen(3000, function() {
+  console.log("Example app listening on port 3000!");
 });
