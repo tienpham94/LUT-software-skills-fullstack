a468dc64a9e39c5f9ad35d694d37d2b5e0149c74
Tien Pham <tiendev94@gmail.com>
2020-07-01 22:05:21 +0300

    Login & Logout

--- exercises-coursework/mean-auth-app/angular-src/src/app/components/login/login.component.ts
+++ exercises-coursework/mean-auth-app/angular-src/src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages'
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  username: String;
+  password: String;
 
-  constructor() { }
+  constructor(private authService: AuthService,
+    private flashMessage: FlashMessagesService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  onLoginSubmit() {
+    const user = {
+      username: this.username,
+      password: this.password
+    }
+
+    this.authService.authenticateUser(user).subscribe(data => {
+      const result = data as any
+      console.log(result)
+      if (result.success) {
+        this.authService.storeUserData(result.token, result.user)
+        this.flashMessage.show('You are now logged in', {cssClass: 'alert-success', timeout: 5000})
+      } else {
+        this.flashMessage.show(result.msg, {cssClass: 'alert-danger', timeout: 5000})
+        this.router.navigate(['login'])
+      }
+    })
   }
 
 }
--- exercises-coursework/mean-auth-app/angular-src/src/app/components/navbar/navbar.component.ts
+++ exercises-coursework/mean-auth-app/angular-src/src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages'
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -7,9 +11,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService,
+    private _flashMessagesService: FlashMessagesService,
+    private router: Router) { }
 
   ngOnInit() {
   }
 
+  onLogoutClick() {
+    this.authService.logout()
+    this._flashMessagesService.show('You are logged out', {cssClass: 'alert-success', timeout: 5000})
+    this.router.navigate(['/login'])
+    return false
+  }
 }
